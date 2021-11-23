6fc34c03a52dd6ea378c81f7e6df48acb8623648
JyotsnaVerma19 <19payal.verma@gmail.com>
2021-11-23 15:45:49 +0000

    Environment testing

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,5 @@
 # Example preprocessing script.
+
+archetype_1 <- cyber.security.1_archetype.survey.responses
+enrolment_1 <- cyber.security.1_enrolments
+
--- src/eda.R
+++ src/eda.R
@@ -1,8 +1,6 @@
 library('ProjectTemplate')
 load.project()
+png(file="C:/Users/Payal/Desktop/Future_Learn_EDA_DM/graphs/test.png",width = 1920, height = 1080)
+ggplot(enrolment_1, aes(gender,fill=gender)) + geom_bar()
+dev.off()
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
