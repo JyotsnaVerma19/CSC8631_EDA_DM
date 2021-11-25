82199003536a414aa6467cc5ce77d5ee9e87d0ad
JyotsnaVerma19 <19payal.verma@gmail.com>
2021-11-25 15:42:19 +0000

    Barchart of gender for learners enrolled

--- src/eda.R
+++ src/eda.R
@@ -3,4 +3,6 @@ load.project()
 png(file="C:/Users/Payal/Desktop/Future_Learn_EDA_DM/graphs/test.png",width = 1920, height = 1080)
 ggplot(enrolment_1, aes(gender,fill=gender)) + geom_bar()
 dev.off()
-
+gender1 = data.frame(enrolment_1$gender[enrolment_1$gender != "Unknown"])
+colnames(gender1) = "gender"
+ggplot() + geom_bar(gender1, mapping = aes(gender,fill=gender))
