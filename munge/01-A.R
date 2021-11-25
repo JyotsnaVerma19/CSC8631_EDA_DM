f1cd1bb724df5d995397ee6713277d8045418b42
JyotsnaVerma19 <19payal.verma@gmail.com>
2021-11-25 15:38:53 +0000

    Loading the data of first batch

--- munge/01-A.R
+++ munge/01-A.R
@@ -2,4 +2,7 @@
 
 archetype_1 <- cyber.security.1_archetype.survey.responses
 enrolment_1 <- cyber.security.1_enrolments
-
+leaving_survey_1 <- cyber.security.1_leaving.survey.responses
+question_response_1 <- cyber.security.1_question.response
+step_activity_1 <- cyber.security.1_step.activity
+weekly_sentiment_1 <- cyber.security.1_weekly.sentiment.survey.responses
